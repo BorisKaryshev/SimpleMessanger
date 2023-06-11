eb57b84634bdf9a725b7370f05e72e2512d44c9d
BorisKaryshev <boris.karyshev@gmail.com>
2023-06-11 15:15:38 +0300

    Wrote some tests

--- tests/SincQueue.cpp
+++ tests/SincQueue.cpp
@@ -3,6 +3,46 @@
 #include <gtest/gtest.h>
 
 TEST(SingleThread, AppendOrder) {
+    constexpr int size = 500;
     Threads::SincQueue<int> queue;
-    ASSERT_TRUE(true);
+
+    for (int i = 0; i < size; ++i) {
+        queue.push(i);
+    }
+
+    for (int i = 0; i < size; ++i) {
+        ASSERT_EQ(queue.pop(), i);
+    }
+}
+
+TEST(SingleThread, Empty) {
+    constexpr int size = 500;
+    Threads::SincQueue<int> queue;
+
+    for (int i = 0; i < size; ++i) {
+        queue.push(i);
+    }
+
+    for (int i = 0; i < 2 * size; ++i) {
+        if (!queue.empty()) {
+            ASSERT_EQ(queue.pop(), i);
+        }
+    }
+}
+
+TEST(MultipleThreads, First) {
+    constexpr int size = 500;
+    Threads::SincQueue<int> queue;
+
+    std::thread thread{[&queue] {
+        for (int i = 0; i < size; ++i) {
+            queue.push(i);
+        }
+    }};
+
+    for (int i = 0; i < size; ++i) {
+        ASSERT_EQ(queue.pop(), i);
+    }
+
+    thread.join();
 }
