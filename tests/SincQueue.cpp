75cd86be74279e5e27a68d13fbda8b3c1a18a5df
BorisKaryshev <boris.karyshev@gmail.com>
2023-06-12 10:50:38 +0300

    Added test which should help find data race

--- tests/SincQueue.cpp
+++ tests/SincQueue.cpp
@@ -2,6 +2,10 @@
 
 #include <gtest/gtest.h>
 
+#include <algorithm>
+#include <numeric>
+#include <vector>
+
 TEST(SingleThread, AppendOrder) {
     constexpr int size = 500;
     Threads::SincQueue<int> queue;
@@ -30,7 +34,7 @@ TEST(SingleThread, Empty) {
     }
 }
 
-TEST(MultipleThreads, First) {
+TEST(MultipleThreads, AddInOneThreadPopInAnother) {
     constexpr int size = 500;
     Threads::SincQueue<int> queue;
 
@@ -45,4 +49,54 @@ TEST(MultipleThreads, First) {
     }
 
     thread.join();
+}
+
+TEST(MultipleThreads, TestForDRD) {
+    constexpr int size = 300;
+    Threads::SincQueue<int> queue;
+
+    std::vector<int> valuePopped;
+    valuePopped.reserve(size);
+
+    std::vector<int> valuePushed;
+    valuePushed.reserve(size);
+
+    auto pusher = [&queue, &valuePushed](int low, int high) -> void {
+        static std::mutex m;
+        for (int i = low; i < high; ++i) {
+            queue.push(i);
+            {
+                std::lock_guard lock{m};
+                valuePushed.push_back(i);
+            }
+        }
+    };
+
+    auto popper = [&valuePopped, &queue](int numOfPops) -> void {
+        static std::mutex m;
+        for (int i = 0; i < numOfPops; ++i) {
+            auto value = queue.pop();
+            {
+                std::lock_guard lock{m};
+                valuePopped.push_back(value);
+            }
+        }
+    };
+
+    std::thread pushers[] = {std::thread(pusher, 0, size / 3), std::thread(pusher, size / 3, 2 * size / 3),
+                             std::thread(pusher, 2 * size / 3, size)};
+
+    std::thread poppers[] = {std::thread(popper, size / 4), std::thread(popper, size / 4), std::thread(popper, size / 4),
+                             std::thread(popper, size / 4)};
+
+    for (auto& thread : pushers) {
+        thread.join();
+    }
+    for (auto& thread : poppers) {
+        thread.join();
+    }
+
+    std::ranges::sort(valuePopped);
+    std::ranges::sort(valuePushed);
+    ASSERT_EQ(valuePopped, valuePushed);
 }
