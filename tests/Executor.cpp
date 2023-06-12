37a67d08bd2559058e7c0c020c35eb2284a700a1
BorisKaryshev <boris.karyshev@gmail.com>
2023-06-12 13:49:50 +0300

    Added tests

--- tests/Executor.cpp
+++ tests/Executor.cpp
@@ -1,3 +1,36 @@
 #include "Executor.hpp"
 
 #include <gtest/gtest.h>
+
+#include <unordered_map>
+
+TEST(Executor, Correct) {
+    constexpr std::size_t numOfThreads = 8;
+    constexpr std::size_t numOfTasks = 100;
+    constexpr std::size_t size = 100;
+
+    std::mutex valuesMutex;
+    std::unordered_map<std::size_t, std::size_t> values;
+
+    auto task = [&values, &valuesMutex] {
+        for (std::size_t i = 0; i < size; ++i) {
+            {
+                std::lock_guard lock{valuesMutex};
+                ++values[i];
+            }
+        }
+    };
+
+    {
+        Threads::Executor exec{numOfThreads};
+
+        for (std::size_t i = 0; i < numOfTasks; ++i) {
+            exec.addTask(task);
+        }
+    }
+
+    ASSERT_EQ(values.size(), size);
+    for (std::size_t i = 0; i < size; ++i) {
+        ASSERT_EQ(values[i], numOfTasks);
+    }
+}
