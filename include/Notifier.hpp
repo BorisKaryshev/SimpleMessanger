8047ad99dcff1fecae813d04ae162480c58f424b
BorisKaryshev <boris.karyshev@gmail.com>
2023-07-07 11:12:36 +0300

    init

--- include/Notifier.hpp
+++ include/Notifier.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <algorithm>
+#include <concepts>
 #include <list>
 #include <ranges>
 
@@ -11,7 +12,7 @@ namespace SiM {
      public:
         class Listener {
          public:
-            virtual auto notify(Args&&... message) -> void = 0;
+            virtual auto notify(const Args&... message) -> void = 0;
         };
 
      public:
@@ -24,10 +25,16 @@ namespace SiM {
         }
 
      protected:
-        auto notifyAll(Args...&& args) -> void {
-            for (auto& listener : m_listeners) {
-                listener->notify(std::forward<Args>(args)...);
-            }
+        auto notifyAll(const Args&... args) -> void {
+            std::ranges::for_each(m_listeners,
+                                  [... args = static_cast<const Args&>(args)](Listener* listener) { listener->notify(args...); });
+        }
+
+        template <typename Pred>
+            requires std::is_invocable_r_v<bool, Pred, Listener*>
+        auto notifyAllIf(Pred&& pred, const Args&... args) -> void {
+            std::ranges::for_each(m_listeners | std::views::filter(pred),
+                                  [... args = static_cast<const Args&>(args)](Listener* listener) { listener->notify(args...); });
         }
 
      private:
