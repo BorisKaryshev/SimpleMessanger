56db1be81a325f6ec118b345a1bcd07dc6a09a9a
BorisKaryshev <boris.karyshev@gmail.com>
2023-07-07 10:37:22 +0300

    Implemented connection class

--- include/TCP.hpp
+++ include/TCP.hpp
@@ -1,7 +1,40 @@
 #pragma once
 
+#include "Notifier.hpp"
+
+#include <boost/asio.hpp>
+
 namespace SiM {
 
-    class TCPConncetion {};
+    /**
+     * @brief Tcp connection. Notifies all listeners on receiving message.
+     * @details Is thread safe. Is noncopyable and nonmovable.
+     */
+    class Connection : public Notifier<std::string> {
+     public:
+        Connection(boost::asio::ip::tcp::socket sock);
+
+        Connection(const Connection&) = delete;
+        auto operator=(const Connection&) -> Connection& = delete;
+
+        auto run() -> void;
+        auto stop() -> void;
+        [[nodiscard]] auto isRunning() const noexcept -> bool;
+
+        auto send(const std::string& text) -> void;
+
+     private:
+        auto m_read() -> void;
+
+     private:
+        constexpr static size_t kiloByte = 1 << 10;
+        constexpr static size_t maxMessageSize = kiloByte << 2;
+
+     private:
+        boost::asio::ip::tcp::socket m_sock;
+        std::atomic_bool m_isRunning{true};
+
+        std::array<char, maxMessageSize> m_acceptedMessage;
+    };
 
 }  // namespace SiM
--- src/TCP.cpp
+++ src/TCP.cpp
@@ -1,3 +1,54 @@
 #include "TCP.hpp"
 
-namespace SiM {}
+#include <iostream>
+
+namespace SiM {
+
+    Connection::Connection(boost::asio::ip::tcp::socket sock) : m_sock(std::move(sock)) {}
+
+    auto Connection::run() -> void {
+        if (m_sock.available()) {
+            m_isRunning = true;
+            m_read();
+        } else {
+            throw std::runtime_error("Connection lost");
+        }
+    }
+
+    auto Connection::stop() -> void {
+        m_sock.close();
+    }
+
+    auto Connection::isRunning() const noexcept -> bool {
+        return m_isRunning;
+    }
+
+    auto Connection::send(const std::string& text) -> void {
+        if (text.size() > maxMessageSize) {
+            throw std::runtime_error("Messages with size more than " + std::to_string(maxMessageSize) + " bytes are not supported");
+        }
+        if (m_sock.is_open()) {
+            m_sock.async_send(boost::asio::buffer(text), []([[maybe_unused]] const boost::system::error_code&, [[maybe_unused]] size_t) {});
+        }
+    }
+
+    auto Connection::m_read() -> void {
+        auto readHandler = [this]([[maybe_unused]] const boost::system::error_code& errorCode, size_t bytes) {
+            constexpr char endLine = '\0';
+
+            if (!errorCode) {
+                m_acceptedMessage[bytes] = endLine;
+                notifyAll(m_acceptedMessage.data());
+            } else if (errorCode == boost::asio::error::operation_aborted) {
+                return;
+            } else {
+                std::cerr << "Error happened " << errorCode << "\n";
+            }
+
+            m_read();
+        };
+
+        m_sock.async_read_some(boost::asio::buffer(m_acceptedMessage), readHandler);
+    }
+
+}  // namespace SiM
