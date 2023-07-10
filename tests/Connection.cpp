57496e6bb4ca409881aeb542672db66e7ba434f3
BorisKaryshev <boris.karyshev@gmail.com>
2023-07-10 05:12:49 +0300

    First working test

--- tests/Connection.cpp
+++ tests/Connection.cpp
@@ -2,7 +2,10 @@
 
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
+#include <boost/asio/executor_work_guard.hpp>
+#include <boost/exception/diagnostic_information.hpp>
 
+#include <fstream>
 #include <list>
 #include <optional>
 #include <string>
@@ -10,128 +13,108 @@
 
 namespace {
 
-    constexpr int port = 8080;
-    constexpr std::string_view stopLiteral = "stop";
+    constexpr int port = 7354;
 
 }  // namespace
 
 namespace {
 
+    std::ofstream logger("log.txt");
+
     template <typename... Args>
     auto print(const Args&... args) -> void {
         std::stringstream stream;
         (stream << ... << args);
-        std::cout << stream.str();
+        logger << stream.str();
     }
 
-    class Store : public SiM::Notifier<std::string>::Listener, public SiM::Connection {
+    class StoreMessages : public SiM::Connection, public SiM::Notifier<std::string>::Listener {
      public:
-        Store(boost::asio::ip::tcp::socket sock) : SiM::Connection(std::move(sock)), m_messages() { addListener(this); }
+        explicit StoreMessages(boost::asio::ip::tcp::socket sock) : SiM::Connection(SiM::Launch::RunManually, std::move(sock)) {
+            addListener(this);
+            run();
+        }
 
         auto notify(const std::string& message) -> void override {
-            std::lock_guard lock(m_listPushAndPop);
-            std::cout << "Got message: " << message << "\n";
+            std::lock_guard lock(m_messagesListOperations);
+            print("Got message: |", message, "|\n");
             m_messages.push_back(message);
         }
 
-        [[nodiscard]] auto lastMessage() const -> std::string {
-            std::lock_guard lock(m_listPushAndPop);
-            return m_messages.back();
+        [[nodiscard]] auto getMessages() const -> const std::list<std::string>& {
+            std::lock_guard lock(m_messagesListOperations);
+            return m_messages;
         }
 
-        [[nodiscard]] auto messages() const noexcept -> const std::list<std::string>& { return m_messages; }
-
      private:
-        mutable std::mutex m_listPushAndPop;
+        mutable std::mutex m_messagesListOperations;
         std::list<std::string> m_messages;
     };
 
-    class Server {
+    class AcceptConnection {
      public:
-        Server(boost::asio::io_context& context) : m_endpoint(boost::asio::ip::tcp::v4(), port), m_acceptor(context, m_endpoint) {}
-
-        auto accept() -> void {
-            auto helper = [this]([[maybe_unused]] const boost::system::error_code& ec, boost::asio::ip::tcp::socket sock) {
-                if (ec) {
-                    print("Error while accepting connection: ", ec, "\n");
-                } else if (!m_connection.has_value()) {
+        explicit AcceptConnection(boost::asio::io_context& context)
+            : m_endpoint(boost::asio::ip::tcp::v4(), port), m_acceptor(context, m_endpoint), m_connection(std::nullopt) {
+            auto handler = [this](const boost::system::error_code& errorCode, boost::asio::ip::tcp::socket sock) {
+                if (errorCode) {
+                    print("ERROR AcceptConnections ", __FILE__, ":", __LINE__, " : ", errorCode.message(), "\n");
+                } else if (!m_connection) {
                     m_connection.emplace(std::move(sock));
-                    m_connection->run();
                 }
             };
 
-            m_acceptor.async_accept(helper);
+            m_acceptor.async_accept(handler);
         }
 
-        auto acceptedMessages() -> std::list<std::string> {
+        [[nodiscard]] auto acceptedMessages() const noexcept -> std::list<std::string> {
             if (m_connection) {
-                return m_connection->messages();
+                return m_connection->getMessages();
             }
             return {};
-        }
-
-        auto stopConnection() -> void {
-            if (m_connection) {
-                m_connection->stop();
-            }
-        }
-
-        [[nodiscard]] auto isRunning() const noexcept -> bool {
-            if (m_connection) {
-                return m_connection->isRunning();
-            }
-            return false;
-        }
-        auto waitForStopLiteral() -> void {
-            while (m_connection && m_connection->lastMessage() != stopLiteral) {
-            }
-            m_connection->stop();
         }
 
      private:
         boost::asio::ip::tcp::endpoint m_endpoint;
         boost::asio::ip::tcp::acceptor m_acceptor;
 
-        std::optional<Store> m_connection;
+        std::optional<StoreMessages> m_connection;
     };
 
     auto workingThread(boost::asio::io_context& context) -> void {
         context.run();
     }
 
+    auto testSendingAndReceivingMessage(const std::list<std::string>& messagesToSend) -> std::list<std::string> {
+        boost::asio::io_context context;
+
+        AcceptConnection server(context);
+
+        std::thread worker{[&context] { workingThread(context); }};
+
+        boost::asio::ip::tcp::endpoint endpoint(boost::asio::ip::address::from_string("127.0.0.1"), port);
+        boost::asio::ip::tcp::socket sock(context);
+        sock.connect(endpoint);
+
+        SiM::Connection connection(SiM::Launch::RunAtConstruction, std::move(sock));
+
+        for (const auto& message : messagesToSend) {
+            connection.send(message);
+        }
+
+        connection.close();
+        worker.join();
+
+        return server.acceptedMessages();
+    }
+
 }  // namespace
 
 using std::string_literals::operator""s;
 
-TEST(First, Some) {
-    boost::asio::io_context clientContext;
-    boost::asio::io_context serverContext;
+using namespace std::chrono_literals;
 
-    Server server(serverContext);
-    server.accept();
+TEST(ClientServerInteraction, SimpleMessages) {
+    std::list<std::string> messages{"Hi, this is the simplest message"s, "More text"s, std::string(1000, 'a')};
 
-    boost::asio::ip::tcp::endpoint ep(boost::asio::ip::address::from_string("127.0.0.1"), port);
-    boost::asio::ip::tcp::socket sock(clientContext);
-    sock.connect(ep);
-    SiM::Connection con(std::move(sock));
-    con.run();
-
-    std::list<std::string> messages{"First"s,
-                                    "second"s,
-                                    "Third"s,
-                                    "Another long one message with some info. Hope it will be useful to you, dear friend"s,
-                                    std::string(2000, 'a'),
-                                    std::string(stopLiteral)};
-
-    std::jthread th{[&serverContext] { workingThread(serverContext); }};
-
-    for (const auto& message : messages) {
-        print("Sending message: ", message, "\n");
-        con.send(message);
-    }
-
-    con.stop();
-    server.waitForStopLiteral();
-
-    ASSERT_EQ(messages, server.acceptedMessages());
+    EXPECT_EQ(messages, testSendingAndReceivingMessage(messages));
 }
